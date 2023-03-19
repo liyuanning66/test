794d91cb60215a36b4bdb769216db829cea3d319
Lyn <1476499819@qq.com>
2023-03-19 16:25:49 +0800

    thread commit

--- src/main/java/com/atguigu/git/Test.java
+++ src/main/java/com/atguigu/git/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("我喜欢吃饭");
         System.out.println("我不喜欢吃饭");
+        System.out.println("我有喜欢吃饭了");
     }
 }
