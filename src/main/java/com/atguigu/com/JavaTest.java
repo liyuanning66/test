9b0b23f32c212cbd826ea412159248ad50e49a33
niyuanning <1476499819@qq.com>
2023-05-29 12:50:15 +0800

    push second2

--- src/main/java/com/atguigu/com/JavaTest.java
+++ src/main/java/com/atguigu/com/JavaTest.java
@@ -4,5 +4,7 @@ public class JavaTest {
     public static void main(String[] args) {
         System.out.println("我要吃饭");
         System.out.println("第二次提交");
+        System.out.println("再一次提交");
+        System.out.println("再次尝试一次");
     }
 }
